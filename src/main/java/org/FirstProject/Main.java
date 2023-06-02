9c9c68c74d2f816afdbb33f0c8bc0a7eaeeae942
mythilipappu <mythilipappu31@gmail.com>
2023-06-02 11:36:38 -0500

    Added new Packages

--- src/main/java/org/FirstProject/Main.java
+++ src/main/java/org/FirstProject/Main.java
@@ -1,7 +1,8 @@
 package org.FirstProject;
 
 public class Main {
+    // Main.java (Example usage)
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello to my first project");
     }
 }
